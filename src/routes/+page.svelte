7f5cfa003f80fe2e484bdb29ae8d7ec1aad7c7a4
Kanathip Pattarakultawee <kanathip137@gmail.com>
2024-10-07 21:07:39 +0900

    new button styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -65,22 +65,25 @@
         <VerticalList bind:items {commands} {deleting}/>
     </table>
     <div id="validation-message"></div>
-    <button on:click={openForm} id="add-button">Add Item</button>
+    <button on:click={openForm} class="button-18 add-button">Add Item</button>
     {#if showForm}
         <div class="popup-form">
             <form on:submit|preventDefault={saveItem}>
-                <label for="label">Label:</label>
-                <input type="text" id="label" bind:value={newItem.label} />
-
-                <label for="url">URL:</label>
-                <input type="url" id="url" bind:value={newItem.url} />
-
-                {#if validationMessage}
-                <div id="validation-message">{validationMessage}</div>
-                {/if}
-
-                <button type="submit">Save</button>
-                <button type="button" on:click={closeForm}>Cancel</button>
+                <div class="container">
+                    <label for="label">Label:</label>
+                    <input type="text" id="label" bind:value={newItem.label} />
+    
+                    <label for="url">URL:</label>
+                    <input type="url" id="url" bind:value={newItem.url} />
+    
+                    {#if validationMessage}
+                    <div id="validation-message">{validationMessage}</div>
+                    {:else}
+                    <div id="validation-message" style="visibility: hidden;">_</div>
+                    {/if}
+                </div>
+                <button type="submit" class="button-18 save-button">Save</button>
+                <button type="button" class="button-18 cancel-button" on:click={closeForm}>Cancel</button>
             </form>
         </div>
     {/if}
@@ -153,42 +156,22 @@
         opacity: 0.5;
         transition: opacity 0.2s;
         visibility: hidden;
-        /* background: url(./remove.svg) no-repeat 50% 50%; */
     }
 
-
-    input {
-        width: 100%;
-        padding: 5px;
-    }
-
-    button {
+    /* button {
         margin: 5px;
         padding: 10px 20px;
-        background-color: #4CAF50;
+        background-color: #439db7;
         color: white;
         border: none;
         cursor: pointer;
-    }
-
-    button#save-button {
-        background-color: #008CBA;
-    }
-
-    button:hover {
-        opacity: 1;
-    }
-
-    button:disabled {
-        background-color: #cccccc;
-        cursor: not-allowed;
-        opacity: 0.6;
-    }
+    } */
 
     .popup-form {
         position: fixed;
         top: 30%;
         left: 50%;
+        width: 20rem;
         transform: translate(-50%, -50%);
         padding: 20px;
         background-color: white;
@@ -197,8 +180,72 @@
         z-index: 1000;
     }
 
+    .container {
+        margin: 0;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .container>* {
+        margin-bottom: 0.7rem;
+    }
+
+    input {
+        padding: 0.2rem;
+    }
+        
+    .button-18 {
+        align-items: center;
+        background-color: #0667c7;
+        border: 0;
+        border-radius: 0.2rem;
+        box-sizing: border-box;
+        color: #ffffff;
+        cursor: pointer;
+        display: inline-flex;
+        justify-content: center;
+        line-height: 1.5rem;
+        max-width: 30rem;
+        min-height: 2.5rem;
+        min-width: 0;
+        overflow: hidden;
+        padding: 0 1.2rem;
+        text-align: center;
+        touch-action: manipulation;
+        transition: background-color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, box-shadow 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s;
+        user-select: none;
+        -webkit-user-select: none;
+        vertical-align: middle;
+    }
+
+    .save-button {
+        background-color: #66c238;
+    }
+    .cancel-button {
+        background-color: grey;
+    }
+
+    .button-18:hover,
+    .button-18:focus {
+        filter: brightness(95%);
+        /* color: #ffffff; */
+    }
+
+    .button-18:active {
+        /* filter: brightness(40%); */
+        color: rgb(255, 255, 255, .7); 
+    }
+   
+
+    .button-18:disabled { 
+        cursor: not-allowed;
+        background: rgba(0, 0, 0, .08);
+        color: rgba(0, 0, 0, .3);
+    }
+
     #validation-message {
         font-weight: bold;
+        color: red;
         margin-bottom: 10px;
     }
 
